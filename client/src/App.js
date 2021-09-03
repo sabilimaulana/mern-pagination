91cc03baa758673c3887c7ad1c7059f9809b7a59
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-03 21:08:57 +0700

    Frontend part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = ({ match }) => {
+  const pageNumber = match.params.pageNumber || 1;
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const [page, setPage] = useState(1);
+  const [pages, setPages] = useState(1);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(`/api/v1/posts?page=${page}`);
+
+        const { data, pages: totalPages } = await res.json();
+
+        setPages(totalPages);
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError(true);
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, [page]);
+
+  console.log(posts);
+  console.log(pages);
+
+  return (
+    <div className="App">
+      {/* Pagination Component */}
+      {/* Posts listing */}
+      {/* Pagination Component */}
+    </div>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route exact path="/page/:pageNumber" component={App} />
+        <Route exact path="/" component={App} />
+      </Switch>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

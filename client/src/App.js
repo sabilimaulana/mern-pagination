c55eb0322ba2b7e43702d7d40dc4969f85716aee
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-05 20:05:37 +0700

    Frontend part 3 || Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./App.css";
 import Card from "./components/Card";
 import Pagination from "./components/Pagination";
 
 const App = ({ match }) => {
+  const history = useHistory();
+
   const pageNumber = match.params.pageNumber || 1;
 
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(+pageNumber);
   const [pages, setPages] = useState(1);
 
   useEffect(() => {
@@ -19,7 +22,7 @@ const App = ({ match }) => {
 
       try {
         const res = await fetch(`/api/v1/posts?page=${page}`);
-
+        history.push(`/page/${page}`);
         const { data, pages: totalPages } = await res.json();
 
         setPages(totalPages);
@@ -27,23 +30,31 @@ const App = ({ match }) => {
         setLoading(false);
       } catch (error) {
         console.log(error);
-        setError(true);
+        setError("Some error occured");
         setLoading(false);
       }
     };
 
     fetchPosts();
-  }, [page]);
+  }, [page, history]);
 
   return (
     <div className="app">
-      <Pagination page={page} pages={pages} changePage={setPage} />
-      <div className="app__posts">
-        {posts.map((post) => (
-          <Card key={post.id} post={post} />
-        ))}
-      </div>
-      <Pagination page={page} pages={pages} changePage={setPage} />
+      {loading ? (
+        <h3 className="loading-text">Loading...</h3>
+      ) : error ? (
+        <h3 className="error-text">{error}</h3>
+      ) : (
+        <>
+          <Pagination page={page} pages={pages} changePage={setPage} />
+          <div className="app__posts">
+            {posts?.map((post, index) => (
+              <Card key={post.id + `${index}`} post={post} />
+            ))}
+          </div>
+          <Pagination page={page} pages={pages} changePage={setPage} />
+        </>
+      )}
     </div>
   );
 };
--- client/src/components/Pagination/index.jsx
+++ client/src/components/Pagination/index.jsx
@@ -1,5 +1,111 @@
-const Pagination = () => {
-  return <div>This is pagination component</div>;
+import "./Pagination.css";
+
+const Pagination = ({ pages, page, changePage }) => {
+  let middlePagination;
+
+  if (pages <= 5) {
+    middlePagination = [...Array(pages)].map((_, idx) => (
+      <button
+        key={idx + 1}
+        onClick={() => changePage(idx + 1)}
+        disabled={page === idx + 1}
+      >
+        {idx + 1}
+      </button>
+    ));
+  } else {
+    const startValue = Math.floor((page - 1) / 5) * 5;
+
+    middlePagination = (
+      <>
+        {[...Array(5)].map((_, idx) => (
+          <button
+            key={startValue + idx + 1}
+            onClick={() => changePage(startValue + idx + 1)}
+            disabled={page === startValue + idx + 1}
+          >
+            {startValue + idx + 1}
+          </button>
+        ))}
+        <button>...</button>
+        <button onClick={() => changePage(pages)}>{pages}</button>
+      </>
+    );
+
+    if (page > 5) {
+      if (pages - page >= 5) {
+        middlePagination = (
+          <>
+            <button onClick={() => changePage(1)}>1</button>
+
+            <button>...</button>
+            <button onClick={() => changePage(startValue)}>{startValue}</button>
+            {[...Array(5)].map((_, idx) => {
+              return (
+                <button
+                  key={startValue + idx + 1}
+                  onClick={() => changePage(startValue + idx + 1)}
+                  disabled={page === startValue + idx + 1}
+                >
+                  {startValue + idx + 1}
+                </button>
+              );
+            })}
+            <button>...</button>
+            <button onClick={() => changePage(pages)}>{pages}</button>
+          </>
+        );
+      } else {
+        const amountLeft = pages - page + 5;
+        middlePagination = (
+          <>
+            <button onClick={() => changePage(1)}>1</button>
+
+            <button>...</button>
+            <button onClick={() => changePage(startValue)}>{startValue}</button>
+            {[...Array(amountLeft)].map((_, idx) => (
+              <button
+                key={startValue + idx + 1}
+                onClick={() => changePage(startValue + idx + 1)}
+                disabled={page === startValue + idx + 1}
+                style={
+                  startValue + idx + 1 > pages
+                    ? {
+                        display: "none",
+                      }
+                    : null
+                }
+              >
+                {startValue + idx + 1}
+              </button>
+            ))}
+          </>
+        );
+      }
+    }
+  }
+
+  return (
+    pages >= 1 && (
+      <div className="pagination">
+        <button
+          className="pagination__prev"
+          onClick={() => changePage((page) => page - 1)}
+          disabled={page === 1}
+        >
+          &#171;
+        </button>
+        {middlePagination}
+        <button
+          className="pagination__next"
+          onClick={() => changePage((page) => page + 1)}
+          disabled={page === pages}
+        >
+          &#187;
+        </button>
+      </div>
+    )
+  );
 };
 
 export default Pagination;
